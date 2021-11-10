8e1c407b61f9886317a1e3f94119e8a2ef6e03e8
paffnkpg <pilotpyjamas@gmail.com>
2021-11-10 11:24:12 +0100

    Functioning MouthMask

---
+++ mouthMask.m
@@ -0,0 +1,29 @@
+function [output_image] = mouthMask(img, faceMask)
+%MOUTHMASK Summary of this function goes here
+%   Detailed explanation goes here
+img = im2double(img);
+img = img.*faceMask;
+
+imshow(img)
+
+imgChrome = rgb2ycbcr(img);
+Cb = mat2gray(imgChrome(:,:,2)) + 1;
+Cr = mat2gray(imgChrome(:,:,3)) + 1;
+
+My = 0.95*mean(Cr(:).^2)./(mean(Cr(:)./Cb(:)));
+
+MouthMap = (Cr.^2).*((Cr.^2) - My*(Cr./Cb)).^2;
+MouthMap = (MouthMap- min(MouthMap(:)))/(max(MouthMap(:)) - min(MouthMap(:)));
+
+se1 = strel('disk', 12);
+
+
+MouthMap = MouthMap > 0.2;
+
+MouthMap = bwareaopen(MouthMap, 200);
+MouthMap = imdilate(MouthMap, se1);
+
+output_image = MouthMap;
+
+end
+
