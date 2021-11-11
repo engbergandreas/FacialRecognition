4ebf9eb97785b08c6f3ace56469e99d272cb95d1
paffnkpg <pilotpyjamas@gmail.com>
2021-11-11 13:15:31 +0100

    Face normalizer

--- main.m
+++ main.m
@@ -1,26 +1,42 @@
-
+%%Tweak image
 %Load faces, start with one face to test
-img = imread('..\DB2/cl_01.jpg');
+img = imread(['..\DB2/cl_08.jpg']);
+%Color correction
 img = colorCorrection(img);
 
 imgD = im2double(img);
+%Track facemask
 faceMask = generateFaceMask(img);
 
+%Eyemask
 eyeMap = eyeMask(imgD);
 
 mult = eyeMap.*faceMask;
 
 imshow(mult.*imgD)
 
-findEyeCoordinates(img)
+%Normalize the face
+eyeCoords=findEyeCoordinates(img);
+%Eyes according to our viewpoint, eyeL is the person's right eye.
+eyeL = eyeCoords(1, :);
+eyeR = eyeCoords(2, :);
 
-%Tweak image
+normalface = normalizeFace(eyeL, eyeR, imgD);
+figure(1)
+imshow(normalface)
+axis on
+hold on;
+% Plot cross at row 100, column 50
+plot(eyeL(1), eyeL(2), 'r+', 'MarkerSize', 30, 'LineWidth', 2);
+
+%Clip face
+
     %Take in a face
-        %Color correction
-        %Track facemask
-        %Eyemask
+
+        
+        
         %eye_mask = eyeMask(img);
-        %Clip face
+        
     %Return facemask
 %The image is tweaked
 
---
+++ normalizeFace.m
@@ -0,0 +1,35 @@
+function [outputImg] = normalizeFace(eyeL, eyeR, inputImg)
+%NORMALIZEFACE Summary of this function goes here
+%   Detailed explanation goes here
+
+% deltaX is the base of the triangle betwwen the eyes
+
+deltaX = eyeR(1) - eyeL(1);
+hypotenuse = norm(eyeL-eyeR);
+
+%Angle betwwen eyes
+degrees = asin(deltaX/hypotenuse);
+
+%The way to rotate depends on which eye is the highest
+if eyeL(2) > eyeR(2)
+    
+    %Padd image so eyeR is the center of the image and use imrotate
+    rotatedImage=rotateAround(inputImg, eyeR(2), eyeR(1), degrees);
+    center = eyeR - [hypotenuse/2, 0];
+
+else
+
+    %Padd image so the eyeL is the center of the image and rotate the other
+    %way.
+    rotatedImage=rotateAround(inputImg, eyeL(2), eyeL(1), degrees);
+    center = eyeL + [hypotenuse/2, 0];
+
+end
+
+Ltop = center + [hypotenuse*-0.9, -hypotenuse*0.45];
+Rbottom = center + [hypotenuse*0.9, hypotenuse*1.8];
+
+outputImg = imcrop(rotatedImage, [Ltop Rbottom-Ltop]);
+
+end
+
