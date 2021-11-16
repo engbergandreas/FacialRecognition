fa0b0a5eff3ec3e1e97fe842c4e219de860baea9
Andreas <adde.engberg@gmail.com>
2021-11-16 16:32:23 +0100

    Add: precompute meanface, eigenfaces and feature vectors

---
+++ precomputeData.m
@@ -0,0 +1,43 @@
+function [meanFace, eigenFaces, weights] = precomputeData(database)
+%[meanFace, eigenFace, weight] = precomputeData(database)
+%meanFace is the mean of all faces in DB
+%eigenFace 
+%weights are the feature vector to compare incoming image to
+
+Settings
+height = NORMALIZED_FACE_HEIGHT;
+width = NORMALIZED_FACE_WIDTH;
+
+images = dir("../DB" + database + "/*.jpg");
+%X images in db as M x N, 1 vector size 
+X = zeros(height * width, length(images));
+%Read every image from database and add them to the matrix X
+for i = 1:length(images)
+img = imread("../DB" + database + "/" + images(i).name);
+img = colorCorrection(img);
+imgD = im2double(img);
+%Normalize the face
+eyeCoords=findEyeCoordinates(img);
+%Eyes according to our viewpoint, eyeL is the person's right eye.
+eyeL = eyeCoords(1, :);
+eyeR = eyeCoords(2, :);
+normalface = normalizeFace(eyeL, eyeR, imgD);
+
+image = rgb2gray(normalface);
+x = reshape(image, height * width, 1);
+
+X(:,i) = x; %face vector
+end
+
+%create avg face 
+meanFace = 1 / length(images) * sum(X,2); %mu
+%imshow(reshape(avgFace, height, width));
+
+diffFace = X - meanFace; %A where each col is phi = xi - mu
+
+C = diffFace' * diffFace; %16x16 matrix of eigenvectors
+
+eigenFaces = diffFace * C; %find the largest eigenvectors u_i of the n x n matrix AA^t 
+
+weights = eigenFaces' * diffFace; %feature vector 
+end
