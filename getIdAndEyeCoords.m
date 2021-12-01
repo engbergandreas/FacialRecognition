528eeafb956ed455555ef6b15604f627df4dd56f
Jakob <jakob.tornqvist@gmail.com>
2021-12-01 20:38:29 +0100

    Test clean up
    Put some redundent code in a function and commented the test in Tqs_testscript

---
+++ getIdAndEyeCoords.m
@@ -0,0 +1,25 @@
+function [id, eyeCoords] = getIdFromImg(imgIn)
+%GETIDFROMIMG Summary of this function goes here
+%   Detailed explanation goes here
+id = 0;
+img = im2double(imgIn);
+img = colorCorrection(img);
+
+eyeCoords = findEyeCoordinates(img);
+
+
+
+if eyeCoords ~= 0
+    eyeL = eyeCoords(1, :);
+    eyeR = eyeCoords(2, :);
+
+    normalface = normalizeFace(eyeL, eyeR, img);
+
+    nrofweights = 8;
+    threshold = 0.2;
+
+    id = recognizeFace(normalface, nrofweights, threshold);
+end
+
+end
+
